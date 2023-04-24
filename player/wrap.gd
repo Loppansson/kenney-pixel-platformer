bc27009fa8996b3e1e4f6333d87826b28521cf0b
Lopano <loppan@e.email>
2023-04-24 15:13:42 +0200

    Added wrap, sort of...

--- player/wrap.gd
+++ player/wrap.gd
@@ -1,3 +1,12 @@
 extends Node
 
-@export var right_limit = 2
+@export var left_limit = -251 
+@export var right_limit = 1315 
+@onready var player = $".."
+
+
+func _physics_process(delta):
+	if player.position.x <= left_limit and player.velocity.x < 0:
+		player.position.x = right_limit
+	if player.position.x >= right_limit and player.velocity.x > 0:
+		player.position.x = left_limit
